d117650eed529644e9c2c33c36aca367addc1f32
tirugit-2001 <tirumaleshaeyadav@gmail.com>
2026-07-06 00:20:28 +0530

    changed video

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ import { usePrefersReducedMotion } from "../../hooks/usePrefersReducedMotion";
 import { useHashScroll } from "../../hooks/useHashScroll";
 import styles from "./home.module.scss";
 import heroImage from "../../assets/images/ChatGPT Image May 29, 2026, 03_13_52 AM.png";
-import heroVideo from "../../assets/videos/Longer version - Hero Section.mp4";
+import heroVideo from "../../assets/videos/Hero Section Video.mp4";
 import ScrollRevealIntro from "../../components/home/ScrollRevealIntro";
 import BuildWithCardsSection from "../../components/home/BuildWithCardsSection";
 // import FeatureTabsSection from "../../components/home/FeatureTabsSection";
